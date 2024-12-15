d86dafc566505ec40b9ccde829d5eb33683ba6d8
NeilPham <huynhnguyenv2@gmail.com>
2024-12-15 14:31:31 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,18 +28,39 @@ export default function RootLayout({
         <link rel="icon" href="static/favicon.ico" />
         <meta property="og:title" content={String(metadata.title) || ""} />
         <meta property="og:description" content={metadata.description || ""} />
-        <meta property="og:type" content="website" />
         <meta
           property="og:url"
-          content="https://my-wedding-gfz1.onrender.com"
+          content="https://my-wedding-gfz1.onrender.com/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Hoàng ❤️ Nguyên" />
+        <meta
+          property="og:description"
+          content="When two hearts become one ❤️"
         />
         <meta
           property="og:image"
-          content="https://my-wedding-gfz1.onrender.com/images/12.JPG"
+          content="https://opengraph.b-cdn.net/production/images/81d6adc6-b84f-42e1-b9f0-d361d1c6750b.png?token=qVKVBdd6qWIIveWQfYtwCcQuhCxPhx0pn_D3X2alFk4&height=1799&width=1200&expires=33270247764"
         />
-        <meta name="og:image:width" content="200" />
-        <meta name="og:image:height" content="300" />
-        <meta name="og:image:type" content="image/jpeg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:domain"
+          content="my-wedding-gfz1.onrender.com"
+        />
+        <meta
+          property="twitter:url"
+          content="https://my-wedding-gfz1.onrender.com/"
+        />
+        <meta name="twitter:title" content="Hoàng ❤️ Nguyên" />
+        <meta
+          name="twitter:description"
+          content="When two hearts become one ❤️"
+        />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/images/81d6adc6-b84f-42e1-b9f0-d361d1c6750b.png?token=qVKVBdd6qWIIveWQfYtwCcQuhCxPhx0pn_D3X2alFk4&height=1799&width=1200&expires=33270247764"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
